a36374abc9fade72ae15800ef5ec47ad81c6b6c3
rowin1102 <rowin1102@naver.com>
2025-07-01 12:45:54 +0900

    2025-07-01

--- FlaskApp/main.py
+++ FlaskApp/main.py
@@ -8,8 +8,6 @@ app = Flask(__name__)
 url = 'http://www.khoa.go.kr/api/oceangrid/tideObsRecent/search.do'
 ServiceKey = 'c2jVwblwmcHB5tOEWxEjbg=='
 
-
-
 @app.route('/')
 def dashboard():
   warning_msg = []
@@ -29,6 +27,16 @@ def api_warning():
     for w in warning:
       warning_msg.append(f"{obs['name']} {w}") 
   return jsonify({'warning' : warning_msg})
+
+@app.route('/api/danger_area')
+def api_danger_area():
+  danger_msg = []
+  for obs in ObsCode:
+    data = get_obs_data(obs, url ,ServiceKey)
+    warning = detect_abnormal(data)
+    if any('위험' in w for w in warning):
+      danger_msg.append(obs['name'])
+  return jsonify({'danger_area' : danger_msg})
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8080, debug=True)
